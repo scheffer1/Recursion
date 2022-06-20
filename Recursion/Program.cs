04909b50253e66d2cf510f2cf2ebfc0cb82280dc
MATEUS SCHEFFERm <mateusscheffer13@gmail.com>
2022-06-20 19:25:54 -0300

    inital commit

---
+++ Recursion/Program.cs
@@ -0,0 +1,16 @@
+﻿public class program
+{
+    static int findSum(int[] a, int N)
+    {
+        if (N <= 0)
+            return 0;
+        return (findSum(a, N - 1) + a[N - 1]);
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] A = { 1, 2, 3, 4, 5 };
+        
+        Console.WriteLine(findSum(A, A.Length));
+    }
+}
