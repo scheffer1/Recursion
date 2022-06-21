278e6872880cfcb6b1dbc35080716c1c0e3d15f7
MATEUS SCHEFFERm <mateusscheffer13@gmail.com>
2022-06-20 21:35:42 -0300

    +largestElement

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,16 +1,25 @@
 ﻿public class program
 {
-    static int findSum(int[] a, int N)
+    static int findSum(int[] array, int N)
     {
         if (N <= 0)
             return 0;
-        return (findSum(a, N - 1) + a[N - 1]);
+        return (findSum(array, N - 1) + array[N - 1]);
     }
 
+    static int largestElement(int[] start, int index) {
+        if (index>0)
+        {
+            return Math.Max(start[index], largestElement(start, index - 1));
+        } else {
+            return start[0];
+        }
+    }
     public static void Main(string[] args)
     {
         int[] A = { 1, 2, 3, 4, 5 };
         
         Console.WriteLine(findSum(A, A.Length));
+        Console.WriteLine(largestElement(A, A.Length-1));
     }
 }
